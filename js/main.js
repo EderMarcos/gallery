4ddb18494aa6414f9aa8ce4cf5ee5ce0fe04a05a
EderMarcos <skrap.marcos@gmail.com>
2017-11-29 19:32:58 -0600

    Documentacion y refactor

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var Gallery = {
+    /**
+     * Atributos
+     * */
     listImages: document.querySelectorAll('#gallery ul li img'),
-    img: null,
     container: document.querySelector("body"),
+    img: null,
     lightbox: null,
     modal: null,
+    /**
+     * Se puede modificar el tipo de animacion (slideLeft, slideTop, fade)
+     * */
     animation: "fade",
 
 
@@ -19,32 +25,44 @@ var Gallery = {
         }
     },
     initLightbox: function (img) {
+        /**
+         * Se obtiene el elemento img
+         * */
         Gallery.img = img.target;
+        /**
+         * Se crea un div para el lightbox y se agrega al body
+         * */
         Gallery.container.appendChild(document.createElement("DIV")).setAttribute("id", "lightbox");
-
+        /**
+         * Busca y obtiene el elemento recien creado
+         * */
         Gallery.lightbox = document.querySelector("#lightbox");
-        Gallery.lightbox.style.width = "100%";
-        Gallery.lightbox.style.height = "100%";
-        Gallery.lightbox.style.position = "fixed";
-        Gallery.lightbox.style.zIndex = "10";
-        Gallery.lightbox.style.background = "rgba(0, 0, 0, .9)";
-        Gallery.lightbox.style.top = 0;
-        Gallery.lightbox.style.left = 0;
+        /**
+         * Se le agrega otro div(Modal) al lightbox
+         * */
         Gallery.lightbox.appendChild(document.createElement("DIV")).setAttribute("id", "modal");
-
+        /**
+         * Busca y obtiene el elemento recien creado
+         * */
         Gallery.modal = document.querySelector("#modal");
+        /**
+         * Se agrega el elemento img y un div que sera el boton de cerrar al div Modal
+         * */
         Gallery.modal.innerHTML = Gallery.img.outerHTML+"<div>x</div>";
-        Gallery.modal.style.display = "block";
-        Gallery.modal.style.position = "relative";
         Gallery.modal.childNodes[0].style.width = "100%";
-        console.log('childNodes', Gallery.modal.childNodes);
 
+        /**
+         * Validacion de tamaño de la pantalla
+         * */
         if (window.matchMedia("(max-width:1000px)").matches) {
             Gallery.modal.style.width = "90%";
         } else {
             Gallery.modal.style.width = "60%";
         }
 
+        /**
+         * Animacion 1
+         * */
         if (Gallery.animation === "slideLeft") {
             Gallery.modal.style.top = "50%";
             Gallery.modal.style.left = 0;
@@ -59,6 +77,9 @@ var Gallery = {
             }, 50)
         }
 
+        /**
+         * Animacion 2
+         * */
         if (Gallery.animation === "slideTop") {
             Gallery.modal.style.top = "-100%";
             Gallery.modal.style.left = "50%";
@@ -73,6 +94,9 @@ var Gallery = {
             }, 50)
         }
 
+        /**
+         * Animacion 3
+         * */
         if (Gallery.animation === "fade") {
             Gallery.modal.style.top = "50%";
             Gallery.modal.style.left = "50%";
@@ -86,24 +110,17 @@ var Gallery = {
             }, 50)
         }
 
-        console.log('Gallery.modal.childNodes[0]', Gallery.modal.childNodes[0]);
-        console.log('Gallery.modal.childNodes[1]', Gallery.modal.childNodes[1]);
-        Gallery.modal.childNodes[1].style.position ="absolute";
-        Gallery.modal.childNodes[1].style.right ="-15px";
-        Gallery.modal.childNodes[1].style.top ="-15px";
-        Gallery.modal.childNodes[1].style.color ="black";
-        Gallery.modal.childNodes[1].style.cursor ="pointer";
-        Gallery.modal.childNodes[1].style.fontSize ="18px";
-        Gallery.modal.childNodes[1].style.width ="30px";
-        Gallery.modal.childNodes[1].style.height ="30px";
-        Gallery.modal.childNodes[1].style.textAlign ="center";
-        Gallery.modal.childNodes[1].style.background ="white";
-        Gallery.modal.childNodes[1].style.borderRadius ="50%";
-
+        /**
+         * Se agrega estilos al boton de cerrar
+         * Se agrega el evento clic al boton cerrar
+         * */
+        Gallery.modal.childNodes[1].className ="btnClose";
         Gallery.modal.childNodes[1].addEventListener("click", Gallery.closeLightbox)
     },
     closeLightbox: function () {
-        console.log(Gallery.lightbox);
+        /**
+         * Elimina el lightbox del body
+         * */
         Gallery.lightbox.parentNode.removeChild(Gallery.lightbox);
     }
 };
